49a8d8c23e47224e41b2d4a0dba88f2875ad450b
enyst <engel.nyst@gmail.com>
2013-08-23 05:36:33 +0300

    Make comments alignment consistent across all files.
    Signed-off-by: enyst <engel.nyst@gmail.com>

--- fileadmin/php/include-files/oscad-model-inc.php
+++ fileadmin/php/include-files/oscad-model-inc.php
@@ -2,21 +2,21 @@
 
 /*  This file is part of OSCAd, the Open Source Compliance Advisor
  *
-*  Copyright (C) 2013 Karsten Reincke, Deutsche Telekom AG
-*
-*  This program is free software: you can redistribute it and/or modify
-*  it under the terms of the GNU Affero General Public License as
-*  published by the Free Software Foundation, either version 3 of the
-*  License, or (at your option) any later version.
+ *  Copyright (C) 2013 Karsten Reincke, Deutsche Telekom AG
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
 
-*  This program is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU Affero General Public License for more details.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
 
-*  You should have received a copy of the GNU Affero General Public License
-*  along with this program.  If not, see <http://www.gnu.org/licenses/>
-*/
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 
 /*** (1) Model ***/
 
@@ -75,7 +75,7 @@ $gOsucLicenseSubpath="unknown-license";
 
 /*
  * Variables being defined by the included open source use case files
-*/
+ */
 
 $gFbOsucNumber="??";
 $gFbOsucName="OSUC-".$gFbOsucNumber;
@@ -105,7 +105,7 @@ $gFbLiSpUcForbidsArr=array();
 /*
  * Reject other methods than GET and POST
  * Note the active parameter variable into $gHttpRequestParameters
-*/
+ */
 
 function checkHttpMethod(){
   global $gHttpRequestMethod,
